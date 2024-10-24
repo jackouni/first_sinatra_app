3953a50f187b1e02acfe307324a3a92ceddac997
Jack Sebben <jacksebben11@hotmail.com>
2024-10-24 15:42:18 -0400

    Added some mock users data and a home page.

--- run_app.rb
+++ run_app.rb
@@ -1,3 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "yaml"
+
+before do 
+  @users_file = File.read("public/users.yaml")
+  @users = YAML.load(user_file)
+end
+
+get "/" do
+
+end
+
