f93e9a75bb45b8864d849f24be6820e736e2bae9
Jack Sebben <jacksebben11@hotmail.com>
2024-10-24 15:09:36 -0400

    Removed some files and cleaned out the run_app.rb file. Going to start from scratch to learn more about Sinatra.

--- run_app.rb
+++ run_app.rb
@@ -1,22 +1,3 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
-
-get "/" do
-  @public_files = Dir.new('public').to_a[2..]
-  @public_files.map! { |file| File.basename(file, ".*") }
-
-  erb :public_files
-end
-
-get "/whodat" do
-  @file_contents = File.read('public/whodat.doc')
-
-  erb :whodat
-end
-
-get "/template" do
-  @file_contents = File.read('public/template.html')
-
-  erb :template
-end
